54c27c68af27e16876b87c0b60df5cd75cd13270
DevExpressExampleBot <fakeEmail>
2025-09-05 12:45:25 +0400

    Source auto update [skip ci]

---
+++ VB/App.xaml.vb
@@ -0,0 +1,20 @@
+' Developer Express Code Central Example:
+' How to enable Drag and Drop feature in the TreeList
+' 
+' This example shows how to enable Drag and Drop functionality in the TreeList
+' View.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E3917
+Imports System.Windows
+
+Namespace How_to_Specify_Nodes_Filter_Mode
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
